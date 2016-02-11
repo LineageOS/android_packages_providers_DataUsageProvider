7030f71b424df121bb3f2518761138ea30c54a96
gkipnis <gkipnis@Garys-MBP.sjc.cyngn.internal>
2016-02-11 14:41:27 -0800

    Initial checkin of the DataUsageProvider
    Implements the provider, service and other misc logic
    for the DataUsage Warning/Disable support that was added to the
    Settings App.
    
    Change-Id: I60735a72adff20624cacd3e27c2d4d0f3a582688

---
+++ Android.mk
@@ -0,0 +1,26 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+
+# LOCAL_JAVA_LIBRARIES := bouncycastle conscrypt telephony-common ims-common
+
+LOCAL_STATIC_JAVA_LIBRARIES := \
+    org.cyanogenmod.platform.sdk \
+    android-support-v4 \
+    android-support-v13 \
+    gson
+
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
+
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+
+LOCAL_PACKAGE_NAME := DataUsageProvider
+LOCAL_CERTIFICATE := platform
+LOCAL_PRIVILEGED_MODULE := true
+
+# include frameworks/opt/setupwizard/navigationbar/common.mk
+# include frameworks/opt/setupwizard/library/common.mk
+# include frameworks/base/packages/SettingsLib/common.mk
+
+include $(BUILD_PACKAGE)
