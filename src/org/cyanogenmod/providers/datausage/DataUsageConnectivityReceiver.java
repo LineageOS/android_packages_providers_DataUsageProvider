f0d7840ad7ceee6e0f0b85584c9ba0d311a1a91d
gkipnis <gkipnis@Garys-MBP.sjc.cyngn.internal>
2016-02-11 14:41:27 -0800

    Initial checkin of the DataUsageProvider
    Implements the provider, service and other misc logic
    for the DataUsage Warning/Disable support that was added to the
    Settings App.
    
    Change-Id: I60735a72adff20624cacd3e27c2d4d0f3a582688

---
+++ src/org/cyanogenmod/providers/datausage/DataUsageConnectivityReceiver.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2016, The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cyanogenmod.providers.datausage;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+
+public class DataUsageConnectivityReceiver extends BroadcastReceiver {
+    private static final String TAG = DataUsageConnectivityReceiver.class.getSimpleName();
+    private static final boolean DEBUG = true;
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // datausage service is only run for metered mobile connections
+        boolean qualified = DataUsageUtils.isDataUsageQualified(context);
+        if (DEBUG) {
+            Log.v(TAG, "onReceived: qualified: " + qualified);
+        }
+        if (qualified) {
+            // start DataUsage service, but only if enabled
+            DataUsageUtils.startDataUsageServiceIfEnabled(context);
+        } else {
+            // stop DataUsage service
+            DataUsageUtils.enableDataUsageService(context, false);
+        }
+    }
+}
